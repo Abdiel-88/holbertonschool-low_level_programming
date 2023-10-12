11e6994a693150d1c609c7d851ae5abe6c333ded
Abdiel-88 <abdiel.f@outlook.com>
2023-10-12 06:08:26 -0700

    script_4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main (void) {
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+ return (0);
 }
