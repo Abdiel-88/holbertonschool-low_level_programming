e198abddad7873ee0b274c129f1f47de11024e36
Abdiel-88 <abdiel.f@outlook.com>
2023-10-12 06:34:01 -0700

    script_5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Decription: This function prints the desired text.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with propper grammar, but the outcome is a piece of art,");
+return (0);
+}
