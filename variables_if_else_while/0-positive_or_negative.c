affbaa381109d64bd887c72a3c3ef3f313d8d269
Abdiel-88 <abdiel.f@outlook.com>
2023-10-16 11:00:51 -0400

    scipt_0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,11 @@ int main(void)
     printf("The number: %d\n", n);
 
     if (n > 0) {
-      printf("is positive\n", n);
+      printf("%d is positive\n", n);
     } else if (n == 0) {
-      printf("is zero\n", n);
+      printf("%d is zero\n", n);
     } else {
-      printf("is negative\n", n);
+      printf("%d is negative\n", n);
     }
 
     return (0);
