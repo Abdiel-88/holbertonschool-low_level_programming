a46dd44beb41d6adfa96aae6ac20bf344b6b6f48
Abdiel-88 <abdiel.f@outlook.com>
2023-10-16 13:09:52 -0400

    script_0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,30 +2,34 @@
 #include <stdio.h>
 #include <time.h>
 
-
 /**
  * main - Entry Point of the program
  *
- * Description: This function prints the size of various types
+ * Description: This function generates a random number and
+ * prints whether it's positive, zero, or negative.
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+    int n;
 
-if (n > 0)
-{
-printf("%d is positive\n", n);
-} else if (n == 0)
-{
-printf("%d is zero\n", n);
-} else
-{
-printf("%d is negative\n", n);
-}
-return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n == 0)
+    {
+        printf("%d is zero\n", n);
+    }
+    else
+    {
+        printf("%d is negative\n", n);
+    }
+
+    return (0);
 }
