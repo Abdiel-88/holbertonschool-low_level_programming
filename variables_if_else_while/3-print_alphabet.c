812bf83bd5c8a1a3a3b8d11b08503125d4343525
Abdiel-88 <abdiel.f@outlook.com>
2023-10-16 19:31:24 -0400

    script_3

---
+++ variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point of the program
+ *
+ * Description: This function generates a random number and
+ * prints whether it's positive, zero, or negative.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z') {
+        putchar(letter);
+        letter++;
+    }
+
+    letter = 'A';
+    while (letter <= 'Z') {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
