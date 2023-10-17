12b42a5ced3281f7e693a08e6c1ac9bc822b1123
Abdiel-88 <abdiel.f@outlook.com>
2023-10-17 10:07:42 -0400

    script_8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,19 +8,22 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-    int number;
-    char letter;
+int main(void)
+{
+	int number;
+	char letter;
 
-    for (number = 0; number < 10; number++) {
-        putchar('0' + number);
-    }
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+	}
 
-    for (letter = 'a'; letter <= 'f'; letter++) {
-        putchar(letter);
-    }
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
 
-    putchar('\n');
-    
-    return 0;
+	putchar('\n');
+
+	return (0);
 }
