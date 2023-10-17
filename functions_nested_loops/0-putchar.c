ff29d2deadf70dde77b4a10d447a95d5029e55b5
Abdiel-88 <abdiel.f@outlook.com>
2023-10-17 13:18:33 -0400

    script_0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include "main.h"
 
-void printPutchar();  /* Function prototype */
-
-int main() {
-    printPutchar();
-    return 0;
+/**
+ * printPutchar - Prints the text "_putchar"
+ *
+ * This function prints the text "_putchar" to the standard output.
+ */
+void printPutchar(void)
+{
+    printf("_putchar\n");
 }
 
-void printPutchar() {
-    printf("_putchar\n");
+/**
+ * main - The main function
+ *
+ * This is the main entry point of the program. It calls the printPutchar
+ * function and returns 0.
+ *
+ * Return: 0 (Exit status)
+ */
+int main(void)
+{
+    printPutchar();
+    return (0);
 }
