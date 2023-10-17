09396bcdcdf91b525da880c711183026061192c9
Abdiel-88 <abdiel.f@outlook.com>
2023-10-17 09:10:10 -0400

    script_6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all single-digit numbers in base 10
+ * starting from 0, followed by a new line, using putchar only twice.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0' + 0);
+	putchar('\n');
+
+	return (0);
+}
