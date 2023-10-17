196251d9da3640b5d894f0f4f1c1a56ab8c91d1f
Abdiel-88 <abdiel.f@outlook.com>
2023-10-17 09:24:24 -0400

    script_7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the lowercase alphabet,
+ * excluding the letters 'e' and 'q', followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter <= 'a')
+	{
+	        
+	  letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
