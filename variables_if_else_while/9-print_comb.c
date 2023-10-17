d28a97db6c99f52b49e6f8e7ca6904fbbcc08e07
Abdiel-88 <abdiel.f@outlook.com>
2023-10-17 10:29:25 -0400

    script_9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,26 +3,26 @@
 /**
  * main - Entry point
  *
- * Description: This program prints all possible combinations of single-digit numbers.
+ * Description: This program prints all single digit combinations.
  * Numbers are separated by a comma and a space, printed in ascending order.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int number;
+	int number;
 
-    for (number = 0; number < 10; number++)
-    {
-        putchar('0' + number);
-        if (number < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+		if (number < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    putchar('\n'); /* Print a newline character */
+	putchar('\n'); /* Print a newline character */
 
-    return (0);
+	return (0);
 }
