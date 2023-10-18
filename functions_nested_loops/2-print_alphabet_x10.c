9dbde172315a22baf490538f37bfd3bc4614fa4c
Abdiel-88 <abdiel.f@outlook.com>
2023-10-18 11:12:26 -0400

    script_2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet ten times followed by a new line.
+ */
+void print_alphabet(void)
+{
+    char letter = 'a';
+    int times = 0;
+    
+    while (times < 10)
+      {
+	while (letter <= 'z')
+	  {
+	    _putchar(letter);
+	    letter++;
+	  }
+      
+
+	_putchar('\n');
+
+	
+	letter = "a";/* Resets letter to "a" for next iteration */
+	times++;
+      }
+}
