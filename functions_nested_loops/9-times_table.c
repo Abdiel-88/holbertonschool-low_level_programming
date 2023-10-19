2b0ae77d292ad68f02b49b8fb2b5f824da4253ba
Abdiel-88 <abdiel.f@outlook.com>
2023-10-19 09:28:01 -0400

    script_9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,29 +5,25 @@
  */
 void times_table(void)
 {
-    int i = 0, j, result;
+    int i, j, result;
 
-    while (i <= 9)
+    for (i = 0; i <= 9; i++)
     {
-        j = 0;
-        while (j <= 9)
+        for (j = 0; j <= 9; j++)
         {
             result = i * j;
 
             if (j > 0)
+            {
                 _putchar(',');
+                _putchar(' ');
+            }
+
             if (result < 10)
                 _putchar(' ');
-            _putchar(' ');
-            if (result >= 10)
-                _putchar(result / 10 + '0');
-            else
-                _putchar(' ');
-            _putchar(result % 10 + '0');
-            
-            j++;
+
+            _putchar('0' + result);
         }
         _putchar('\n');
-        i++;
     }
 }
