3f20361431e856ac92884ccace680cfd9c01030e
Abdiel-88 <abdiel.f@outlook.com>
2023-10-19 09:50:26 -0400

    script_10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+include "main"
+
+/**
+ * add - Adds two integers.
+ * @a: the first interger.
+ * @b: the second integer.
+ *
+ * return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
