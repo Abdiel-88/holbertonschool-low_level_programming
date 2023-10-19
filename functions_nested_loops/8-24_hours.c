e36e826c4c3d8b17eb07661f371b1b6f53a133a8
Abdiel-88 <abdiel.f@outlook.com>
2023-10-19 09:12:38 -0400

    script_8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,18 +5,18 @@
  */
 void jack_bauer(void)
 {
-    int hour, minute;
+int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar('0' + (hour / 10));  /* Print tens digit of the hour */
-            _putchar('0' + (hour % 10));  /* Print ones digit of the hour */
-            _putchar(':');                /* Print the colon */
-            _putchar('0' + (minute / 10)); /* Print tens digit of the minute */
-            _putchar('0' + (minute % 10)); /* Print ones digit of the minute */
-            _putchar('\n');                /* Print a newline character */
-        }
-    }
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar('0' + (hour / 10));  /* Print tens digit of the hour */
+_putchar('0' + (hour % 10));  /* Print ones digit of the hour */
+_putchar(':');                /* Print the colon */
+_putchar('0' + (minute / 10)); /* Print tens digit of the minute */
+_putchar('0' + (minute % 10)); /* Print ones digit of the minute */
+_putchar('\n');                /* Print a newline character */
+}
+}
 }
