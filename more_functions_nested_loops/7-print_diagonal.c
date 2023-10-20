6136fc69a51f6e075dfad0b99fbe9829911cc952
Abdiel-88 <abdiel.f@outlook.com>
2023-10-20 18:53:11 -0400

    script_7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,24 @@
  */
 void print_diagonal(int n)
 {
-    int i = 0;
+int i = 0;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    while (i < n)
-    {
-        int j = 0;
-        while (j < i)
-        {
-            _putchar(' ');
-            j++;
-        }
-        _putchar('\\');
-        _putchar('\n');
-        i++;
-    }
+while (i < n)
+{
+int j = 0;
+while (j < i)
+{
+_putchar(' ');
+j++;
+}
+_putchar('\\');
+_putchar('\n');
+i++;
+}
 }
