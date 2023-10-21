3d410472e090656c58814a370d0d8446843e2358
Abdiel-88 <abdiel.f@outlook.com>
2023-10-21 14:22:54 -0400

    script_9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,23 +5,30 @@
  *
  * Return: 0 (Success)
  */
-int main(void)
-{
-    int i;
-
-    for (i = 1; i <= 100; i++)
+int main()
+  for (int num =1; num <= 100; num++)
     {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz ");
-        else if (i % 3 == 0)
-            printf("Fizz ");
-        else if (i % 5 == 0)
-            printf("Buzz ");
-        else
-            printf("%d ", i);
+      if (num % 3 == 0 && num % 5 == 0)
+	{
+	  printf("FizzBuzz");
+	}
+      else if (num % 3 == 0)
+	{
+	  printf("Fizz");
+	}
+      else if (num % 5 == 0)
+	{
+	  printf("Buzz");
+	}
+      else
+	{
+	  printf("%d", num);
+	}
+      if (num < 100)
+	{
+	  printf(" ");
+	}
     }
-
-    printf("\n");
-
-    return (0);
+printf("\n");
+return (0);
 }
