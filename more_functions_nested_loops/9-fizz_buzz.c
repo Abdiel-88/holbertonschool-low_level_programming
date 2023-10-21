cc99c9563dde073d91ae1c374000245500398d6d
Abdiel-88 <abdiel.f@outlook.com>
2023-10-21 14:37:59 -0400

    script_9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 
-int main()
-{
-  int num;  
+/**
+ * main - Prints numbers from 1 to 100 with Fizz-Buzz rules.
+ *
+ * Return: 0 (Success)
+ */
 
-  for (num = 1; num <= 100; num++)
-  {
-    
-    if (num % 3 == 0 && num % 5 == 0)
-    {
-      printf("FizzBuzz");
-    }
-    else if (num % 3 == 0)
-    {
-      printf("Fizz");
-    }
-    else if (num % 5 == 0)
-    {
-      printf("Buzz");
-    }
-    else
-    {
-      printf("%d", num);
-    }
-    if (num < 100)
-    {
-      printf(" ");
-    }
-  }
-  printf("\n");
-  return 0;
+int main(void)
+{
+int num;
+
+for (num = 1; num <= 100; num++)
+{
+
+if (num % 3 == 0 && num % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (num % 3 == 0)
+{
+printf("Fizz");
+}
+else if (num % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", num);
+}
+if (num < 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
 }
