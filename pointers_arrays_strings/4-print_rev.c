bf492e78eac2709355d1b45279487fd0d3a7ed1e
Abdiel-88 <abdiel.f@outlook.com>
2023-10-24 11:32:06 -0400

    reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,17 +4,17 @@
  * print_rev - Prints a string in reverse order followed by a new line.
  * @s: A pointer to the string to be printed in reverse.
  */
-void print_rev(char *s) {
-    int length = 0;
-    int i;  // Declare the loop variable outside the loop
+void print_rev(char *s)
+{
+int length = 0;
 
-    while (s[length] != '\0') {
-        length++;
-    }
-
-    for (i = length - 1; i >= 0; i--) {
-        putchar(s[i]);
-    }
-
-    putchar('\n');
+while (s[length] != '\0')
+{
+length++;
+}
+for (int i = length - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
 }
