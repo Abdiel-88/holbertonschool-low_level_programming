ccbd06c35af4c629fcd3dc3381caa1520adb9fef
Abdiel-88 <abdiel.f@outlook.com>
2023-10-25 15:59:07 -0400

    rev_array

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,6 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif /* End */
