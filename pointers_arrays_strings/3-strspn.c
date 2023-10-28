b1880da8895492b32476cc0413c0227514c23776
Abdiel-88 <abdiel.f@outlook.com>
2023-10-28 10:51:49 -0400

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,8 +11,8 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int count = 0; 
-int i = 0; 
+unsigned int count = 0;
+int i = 0;
 
 while (s[i] != '\0')
 {
@@ -27,5 +27,5 @@ break;
 i++;
 }
 
-return (count); 
+return (count);
 }
