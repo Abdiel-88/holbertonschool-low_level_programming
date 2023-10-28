4d715c1c6e1431355ecc15cadc955dad2366271c
Abdiel-88 <abdiel.f@outlook.com>
2023-10-28 10:05:47 -0400

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,23 @@
 #include "main.h"
 #include <stddef.h>
 
-unsigned int _strspn(char *s, char *accept) {
-    unsigned int count = 0; // Initialize the counter
-    int i = 0; // Initialize the loop counter
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0; 
+int i = 0; 
 
-    while (s[i] != '\0') {
-        if (strchr(accept, s[i]) != NULL) {
-            // The current character from s is in accept, increment the count
-            count++;
-        } else {
-            // The current character from s is not in accept, stop counting
-            break;
-        }
-        i++; // Move to the next character in s
-    }
-
-    return count; // Return the count of acceptable characters
+while (s[i] != '\0')
+{
+if (strchr(accept, s[i]) != NULL)
+{
+count++;
 }
-	
+else
+{
+break;
+}
+i++;
+}
+
+return (count); 
+}
