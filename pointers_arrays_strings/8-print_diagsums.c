6b92e2a2b81a3c05eb23918e55324536bbf3c401
Abdiel-88 <abdiel.f@outlook.com>
2023-10-28 12:12:37 -0400

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -18,6 +18,6 @@ diag1_sum += a[i * size + i];
 diag2_sum += a[i * size + (size - 1 - i)];
 }
 
-printf("Sum of diagonal 1: %d\n", diag1_sum);
-printf("Sum of diagonal 2: %d\n", diag2_sum);
+printf("Sum of diagonal 1: %d, Sum of diagonal 2: %d\n", diag1_sum, diag2_sum);
+
 }
