2b18c26e9f1b67687ba1b0db5a8112a9777227fe
Abdiel-88 <abdiel.f@outlook.com>
2023-10-30 17:38:45 -0400

    recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Prints a string using recursion, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+printf('\n');
+}
+else
+{
+putchar(*s);
+_puts_recursion(s + 1);
+}
+}
+/**
+ * main - Entry point of the program.
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+char *str = "Hello, World";
+_puts_recursion(str);
+return (0);
+}
