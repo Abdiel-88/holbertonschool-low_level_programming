fe1a1b5b739b06fa165fb795ac1e3de4787d96d2
Abdiel-88 <abdiel.f@outlook.com>
2023-11-10 12:06:44 -0400

    macro.h

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * file 3-function_like_macro.h
+ * author OpenAI Assistant
+ * description: Function-like macro ABS(x) for computing the absolute value of x
+ */
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
