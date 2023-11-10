68c69baf81fe82dd28d8c888e74e4f73b9584139
Abdiel-88 <abdiel.f@outlook.com>
2023-11-09 23:31:01 -0400

    macro.h

---
+++ preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * file 0-object_like_macro.h
+ * author OpenAI Assistant
+ * description: Macro definition for SIZE
+ */
+
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
