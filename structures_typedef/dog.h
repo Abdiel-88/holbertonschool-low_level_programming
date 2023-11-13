2c305d141f9ec349cca88b6c7ad373c52bc1c3e2
Abdiel-88 <abdiel.f@outlook.com>
2023-11-13 16:07:28 -0400

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * struct dog - a dog's info.
+ * @name - a dog's name.
+ * @age - a dog's age.
+ * @owner - a dog's owner.
+ */
+
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
