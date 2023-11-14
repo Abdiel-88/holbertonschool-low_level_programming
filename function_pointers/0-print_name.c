b40361e3bb68fbf3adeccab18b017eb6a5827895
Abdiel-88 <abdiel.f@outlook.com>
2023-11-14 19:31:40 -0400

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -10,10 +10,26 @@
  *              an argument to that function. The function pointed to by 'f'
  *              should handle the actual printing of the name.
  */
-void print_name(char *name, void (*f)(char *))
-{
-    if (name != NULL && f != NULL)
-      {
-        f(name);
+void print_name(char *name, void (*f)(char *)) {
+    // Check if the name and the function pointer are not NULL
+    if (name != NULL && f != NULL) {
+        f(name); // Call the function pointed to by 'f' with 'name' as its argument
     }
+}
+
+/**
+ * print_to_console - Prints a string to the console.
+ * @str: The string to print.
+ *
+ * Description: This function takes a string and prints it to the console.
+ */
+void print_to_console(char *str) {
+    printf("%s\n", str); // Simply prints the string to the console
+}
+
+int main() {
+    char *name = "Alice";
+    print_name(name, print_to_console); // This will print "Alice" to the console
+
+    return 0;
 }
