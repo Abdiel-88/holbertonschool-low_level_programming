b2cfa12c04154ffc789abc9efcfb9b4d5d083ece
Abdiel-88 <abdiel.f@outlook.com>
2023-11-15 12:10:19 -0400

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * print_name - Prints a name using a given print function.
  * @name: The name to print.
  * @f: A pointer to a function that takes a char* (string) and returns nothing.
  *
  * Description: This function takes a string and a function pointer.
- *              It calls the function pointed to by 'f', passing 'name' as
- *              an argument to that function. The function pointed to by 'f'
- *              should handle the actual printing of the name.
+ * It calls the function pointed to by 'f', passing 'name' as
+ * an argument to that function. The function pointed to by 'f'
+ * should handle the actual printing of the name.
  */
-void print_name(char *name, void (*f)(char *)) {
-    // Check if the name and the function pointer are not NULL
-    if (name != NULL && f != NULL) {
-        f(name); // Call the function pointed to by 'f' with 'name' as its argument
-    }
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+{
+f(name);
+}
 }
 
 /**
@@ -24,13 +24,15 @@ void print_name(char *name, void (*f)(char *)) {
  *
  * Description: This function takes a string and prints it to the console.
  */
-void print_to_console(char *str) {
-    printf("%s\n", str); // Simply prints the string to the console
+void print_to_console(char *str)
+{
+printf("%s\n", str);
 }
 
-int main() {
-    char *name = "Alice";
-    print_name(name, print_to_console); // This will print "Alice" to the console
+int main()
+{
+char *name = "Alice";
+print_name(name, print_to_console);
 
-    return 0;
+return (0);
 }
