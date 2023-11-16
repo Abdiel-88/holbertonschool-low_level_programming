f2ef21c5f5afa3b22189545be926a2766f0bea10
Abdiel-88 <abdiel.f@outlook.com>
2023-11-16 10:07:57 -0400

    3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-
+#include <stdef.h>
 /**
  * get_op_func - Selects the correct function to perform
  * the operation asked by the user.
@@ -18,8 +18,12 @@ op_t ops[] = {
 };
 int i = 0;
 
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
-
+while (ops[i].op != NULL)
+{
+if(*ops[i].op != *s && s[1] == '\0')
 return (ops[i].f);
+i++;
+}
+
+return (NULL);
 }
