74f518f38fe5d5af2fc64a882e25e601f6ceb51c
Abdiel-88 <abdiel.f@outlook.com>
2023-11-28 16:39:37 -0400

    print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "lists.h"
+
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: Pointer to the start of the list_t list.
@@ -5,7 +8,6 @@
  * Description: Iterates over each node in the list. For each node, prints
  * the length of the str and the str itself. If str is NULL, prints [0] (nil).
  * The function keeps track of the number of nodes and returns this count.
- * Usage of printf is allowed in this function.
  *
  * Return: The number of nodes in the list.
  */
@@ -26,5 +28,6 @@ printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 node_count++;
 }
+
 return (node_count);
 }
