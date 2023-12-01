58380bc9257d19ab9d12469c519592749cf46114
Abdiel-88 <abdiel.f@outlook.com>
2023-12-01 13:53:04 -0400

    print binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to be printed in binary
+ */
+void print_binary(unsigned long int n)
+{
+if (n > 1) {  /* Recur until we reach the most significant bit */
+print_binary(n >> 1);
+}
+_putchar((n & 1) + '0'); /* Print the least significant bit of current frame */
+}
