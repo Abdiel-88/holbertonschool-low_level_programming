e698bd1e26a0d0308e4d468aed95982afaf9a1d7
Abdiel-88 <abdiel.f@outlook.com>
2023-12-01 14:08:10 -0400

    set bit

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,7 +9,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= sizeof(*n) * 8) {
+if (index >= sizeof(*n) * 8)
+{
 return (-1); /* Return -1 if index is out of range */
 }
 *n |= (1UL << index); /* Set the bit at 'index' to 1 */
