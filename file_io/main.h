fb4aa8adc2a35e467596dacdc4803d99f3fde115
Abdiel-88 <abdiel.f@outlook.com>
2023-12-07 12:26:38 -0400

    read text

---
+++ file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>  // For ssize_t and size_t
+
+/* Function Prototypes */
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters it should read and print.
+ *
+ * Return: The actual number of letters it could read and print, or 0 if fails.
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
