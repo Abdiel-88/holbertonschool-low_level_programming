77b742d6b822a4399b739543d8e8a4a7173e0551
Abdiel-88 <abdiel.f@outlook.com>
2023-12-08 11:17:30 -0400

    djb2

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * hash_djb2 - implements the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ *
+ * Description: This function takes a string and computes a hash value using
+ *              the djb2 algorithm. The algorithm has been proven to have
+ *              good distribution and speed on many different sets of keys.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash = 5381;
+int c;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+}
+return (hash);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -43,6 +43,7 @@ hash_node_t **array; /* An array of size @size */
 
 /* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 /* Other function prototypes would go here, for example: */
 /* int hash_table_set(hash_table_t *ht, const char *key, const char *value); */
