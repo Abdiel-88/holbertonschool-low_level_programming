49c7f45c820407209dd91be73bdfce7c526c2f1c
Abdiel-88 <abdiel.f@outlook.com>
2024-01-16 21:07:51 -0400

    task 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -9,16 +9,16 @@
  */
 void print_array(const int *array, size_t left, size_t right)
 {
-    size_t i;
+size_t i;
 
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-    {
-        if (i > left)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
+printf("Searching in array: ");
+for (i = left; i <= right; i++)
+{
+if (i > left)
+printf(", ");
+printf("%d", array[i]);
+}
+printf("\n");
 }
 
 /**
@@ -32,24 +32,24 @@ void print_array(const int *array, size_t left, size_t right)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+size_t left = 0;
+size_t right = size - 1;
+size_t mid;
 
-    if (array == NULL)
-        return -1;
+if (array == NULL)
+return (-1);
 
-    while (left <= right)
-    {
-        print_array(array, left, right);
-        mid = left + (right - left) / 2;
-        if (array[mid] == value)
-            return (int)mid;
-        if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+while (left <= right)
+{
+print_array(array, left, right);
+mid = left + (right - left) / 2;
+if (array[mid] == value)
+return (int)mid;
+if (array[mid] < value)
+left = mid + 1;
+else
+right = mid - 1;
+}
 
-    return -1;
+return (-1);
 }
